f9519b5c2ab960f8639533d48b04b74072cc9156
kota7175 <pengon13064@gmail.com>
2021-12-01 23:50:36 +0900

    ホットリロードができなかったので修正

--- homepage/next.config.js
+++ homepage/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  /**windows上のDockerでホットリロードを適用させる */
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
